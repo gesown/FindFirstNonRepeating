280fced3cc3b46ec693ff11e8e1f47b1169a1bb0
gesown <owner@groganenterpriseservices.com>
2019-04-18 23:12:11 -0700

    Initial commit.

--- FindFirstNonRepeating/Program.cs
+++ FindFirstNonRepeating/Program.cs
@@ -10,6 +10,27 @@ namespace FindFirstNonRepeating
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: FindFirstNonRepeating {string} -> writes character to console." );
+            }
+            var stringIn = args[0];
+             var stringChars = stringIn.ToList();
+            HashSet<char> hashSet=new HashSet<char>(stringChars);
+            if (hashSet.Count == stringChars.Count)
+            {
+                Console.WriteLine("First Non-repeating char: "+hashSet.First());
+                return;
+            }
+            IDictionary<char,int> strDictionary=new Dictionary<char, int>(hashSet.Count);
+            foreach (var c in hashSet)
+            {
+                var cCount = stringChars.Count(s => s == c);
+                strDictionary.Add(c,cCount);
+            }
+
+            var firstOne = strDictionary.First(s => s.Value == 1).Key;
+            Console.WriteLine("First Non-repeating char: " + firstOne);
         }
     }
 }
